7a9bcb3b697189e8b4dade078b71edb2ab6e7de8
Stefánia Péter <lullaby@schatten.hu>
2017-11-29 20:13:21 +0100

    Some random fixes I forgot to commit previously.

--- head.php
+++ head.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <base href="/">
     <title>Stefánia Péter / Pixel forger / Cat lover</title>
     <meta name="author" content="Stefánia Péter"/>
     <meta name="description" content="Portfolio of Stefánia Péter"/>
+    <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon.png">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@cat_a_flame">
+    <meta name="twitter:creator" content="@cat_a_flame">
+    <meta name="twitter:url" content="https://schatten.hu">
+    <meta name="twitter:title" content="About Stefánia Péter">
+    <meta name="twitter:description" content="Stefánia Péter's portfolio website">
+    <meta name="twitter:image" content="/img/favicon.png">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,400i|Playfair+Display:400,700|Roboto+Condensed:400,700&amp;subset=latin-ext" rel="stylesheet"/>
     <link rel="stylesheet" href="/dist/css/main.css" />
     <script src="/dist/js/main.js"></script>
