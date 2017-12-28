0202d8cbd6fad0be68e0985c419a5e3d57058624
Stefánia Péter <lullaby@schatten.hu>
2017-12-28 12:39:44 +0100

    Some random text updates.

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="footer_container">
         <p class="footer_container_text">
-            Made with love and Sublime Text - schatten.hu &copy; <?php echo date("Y"); ?> - A <a class="footer_container_text_link" href="https://github.com/Stefanye/schatten.hu" target="_blank">Github</a> project.
+            Made with love - schatten.hu &copy; <?php echo date("Y"); ?> - A <a class="footer_container_text_link" href="https://github.com/Stefanye/schatten.hu" target="_blank">Github</a> project.
         </p>
     </div>
 </footer>
