e1dc942b3f0db7652ba8f0cfa1d133db718bf8cf
Stefánia Péter <lullaby@schatten.hu>
2019-06-29 13:11:07 +0200

    Google analytics update.

--- head.php
+++ head.php
@@ -4,12 +4,14 @@
     <meta charset="UTF-8">
     <base href="/">
     <title>Stefánia Péter / Pixel forger / Cat lover</title>
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-4677027-3"></script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-143050469-1"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
-        gtag('config', 'UA-4677027-3');
+
+        gtag('config', 'UA-143050469-1');
     </script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="author" content="Stefánia Péter"/>
