323ae434f9e9e4a062b29d156430d24158e7d9b9
Stefánia Péter <stefania.peterx@gmail.com>
2021-04-24 18:02:53 +0200

    Refactor the whole site

--- footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer">
-    <div class="footer_container">
-        <p class="footer_container_text">
-            Made with love - schatten.hu &copy; <?php echo date("Y"); ?> - A <a class="footer_container_text_link" href="https://github.com/Stefanye/schatten.hu" target="_blank">Github</a> project.
-        </p>
-    </div>
-</footer>
-</body>
-</html>
--- index.php
+++
@@ -1,9 +0,0 @@
-<?php include('head.php') ?>
-
-<main class="content" role="main">
-    <div class="content_container">
-        <?php include('info.php') ?>
-    </div>
-</main>
-
-<?php include 'footer.php';?>
