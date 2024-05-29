27d884f96b850ac829e79a2e11eb6112ff2da98a
Stefania Peter <1927103+cat-a-flame@users.noreply.github.com>
2024-05-29 11:58:42 +0200

    Add chapters

--- src/js/chapters.js
+++ src/js/chapters.js
@@ -1,17 +1,39 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const chapters = document.querySelectorAll('article h2');
-    const sidebar = document.getElementById('chapterSidebar');
-  
-    chapters.forEach((chapter, index) => {
-      const listItem = document.createElement('li');
-      listItem.textContent = chapter.textContent;
-      //listItem.dataset.target = `chapter-${index}`;
-      sidebar.appendChild(listItem);
-  
-      // Add an id to each chapter for easy reference
-      //chapter.id = `chapter-${index}`;
-    });
-  
-    const sidebarItems = sidebar.querySelectorAll('li');
+document.addEventListener('DOMContentLoaded', function () {
+  const chapters = document.querySelectorAll('article h2');
+  const sidebar = document.getElementById('chapterSidebar');
+
+  chapters.forEach((chapter, index) => {
+    const chapterId = chapter.getAttribute('id') || `chapter-${index + 1}`;
+    chapter.setAttribute('id', chapterId)
+    const listItem = document.createElement('li');
+    const link = document.createElement('a');
+    link.setAttribute('href', `#${chapterId}`);
+    link.setAttribute('id', `link-${chapterId}`);
+    link.textContent = chapter.textContent;
+    listItem.appendChild(link);
+    sidebar.appendChild(listItem);
   });
-  
+
+  const links = document.querySelectorAll('.sidebar a');
+
+  function handleScroll() {
+    let closestChapterIndex = 0;
+    let closestDistance = Number.POSITIVE_INFINITY;
+
+    chapters.forEach((chapter, index) => {
+      const rect = chapter.getBoundingClientRect();
+      const distanceFromTop = Math.abs(rect.top);
+
+      if (distanceFromTop < closestDistance) {
+        closestDistance = distanceFromTop;
+        closestChapterIndex = index;
+      }
+    });
+
+    links.forEach(link => link.classList.remove('active'));
+    links[closestChapterIndex].classList.add('active');
+  }
+
+  document.addEventListener('scroll', handleScroll);
+  handleScroll();
+});
